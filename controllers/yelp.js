fec411d1b6b2cbf036fd1a3887f87290b51b36cf
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-07-21 17:25:15 -0500

    Added yelp route

--- controllers/yelp.js
+++ controllers/yelp.js
@@ -1,4 +1,20 @@
+//Require the express package and use express.Router()
 const express = require('express');
 const router = express.Router();
 const url = "https://api.yelp.com/v3/businesses/search"
 
+console.log('wtf');
+router.get('/', function(req, res){ 
+    request(url , function (error, response, body) { 
+      if (!error && response.statusCode === 200) { 
+        console.log('hiiiiiiiiiiiiiiii'); 
+        res.send(body); 
+      }
+     }); 
+  });
+
+  router.get('/test',function(req,res){
+      res.send('hi')
+  })
+
+  module.exports = router;
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//Initialize required modules
 const express=  require('express');
 const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
+const yelp = require('./controllers/yelp')
 const app = express();
 const port = 3000;
 
@@ -11,11 +13,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}))
 //middleware for bodyparsing using urlencoding
 app.use(bodyParser.json())
-
+ app.use('/yelp',yelp)
 /*express.static is a built in middleware function to serve static files.
  We are telling express server public folder is the place to look for the static files*/
  app.use(express.static(path.join(__dirname, 'public')));
-
  app.get('/',(req,res,next) => { // es6 syntax (function (req,res,next){})
     res.send('invalidpage');
 })

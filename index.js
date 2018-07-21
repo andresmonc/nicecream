8f9c4f44c9a76584d0f18cb967e21889bbe7d4a7
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-07-21 16:57:45 -0500

    Added a missing require for 'path'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=  require('express');
 const cors = require('cors');
-const path = ('path');
+const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;

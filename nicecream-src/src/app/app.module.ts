bbe84ca57fa4e30fbabaea3dabacb2c162eb4a9e
Farhan <farhan.t.islam@gmail.com>
2018-07-21 16:11:19 -0700

    added angular material/cdk and results page component

--- nicecream-src/src/app/app.module.ts
+++ nicecream-src/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ResultsPageComponent } from './results-page/results-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ResultsPageComponent
   ],
   imports: [
     BrowserModule

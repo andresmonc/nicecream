24a6d42b502617e689eedd2e877a1e0f018d0c9d
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-07-21 19:14:48 -0500

    connected Express server to yelp api

--- controllers/yelp.js
+++ controllers/yelp.js
@@ -1,26 +1,18 @@
 //Require the express package and use express.Router()
 const express = require('express');
 const router = express.Router();
-const url = "https://api.yelp.com/v3/businesses/search";
+// const url = "https://api.yelp.com/v3/businesses/search";
 var request = require('request');
-console.log('wtf');
-
-router.get('/', function(req, res){ 
-    request(url , function (error, response, body) { 
-      if (!error && response.statusCode === 200) { 
-        console.log('hiiiiiiiiiiiiiiii'); 
-        res.send(body); 
+var options = {
+    url: 'https://api.yelp.com/v3/businesses/search?location=dallas&term=ice cream',
+    headers: {
+        'Authorization': 'Bearer Pv1lU27daPJgP-05n3gKusrl7WJ9tEb2r6EavLMsBQ7KQld62B_Q-J3_OgaB8NtdFe8ZDk4qh_opXjwb0Vj0Chhp9yZCLcjCLXQJOe02_z4BZd7ue1mhVQOaIN9QW3Yx'
       }
-     }); 
-  });
-
-
-  router.get('/test',function(req,res){
-
-    request(url , function (error, response, body){
+};
+router.get('/', function (req,res) {
+    request(options, function(error,response,body) {
         res.send(body);
     });
-})
+});
 
-
-  module.exports = router;
+module.exports = router;

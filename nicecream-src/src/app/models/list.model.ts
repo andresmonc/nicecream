d5f16bee2c54b19726dcecb0ce7cacac04381098
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-07-23 12:29:39 -0500

    Modified list service to pull from localhost server api and passed data to results-page component; need to build list model

---
+++ nicecream-src/src/app/models/list.model.ts
@@ -0,0 +1,7 @@
+export default class List {
+    constructor(
+        //object goes here
+    ){}
+
+
+}
--- nicecream-src/src/app/services/list.service.ts
+++ nicecream-src/src/app/services/list.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import List from '../models/list.model'
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListService {
+  private url = 'http://localhost:3000/yelp';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getList(): Observable<List[]> {
+    return this.http.get<List[]>(this.url);
+  }
 }
+
+

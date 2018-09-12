2b6c3850f3d0a4c37fec6d0590c7ba24bcb6e8a8
Jaime Moncayo <jaimeandresmonc@gmail.com>
2018-09-12 16:39:18 -0500

    minor code refactor to index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express=  require('express');
 const cors = require('cors');
 const path = require('path');
 const bodyParser = require('body-parser');
-const yelp2 = require('./controllers/yelp')
+const yelp = require('./controllers/yelp')
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}))
 //middleware for bodyparsing using urlencoding
 app.use(bodyParser.json())
- app.use('/yelp',yelp2)
+ app.use('/yelp',yelp)
 /*express.static is a built in middleware function to serve static files.
  We are telling express server dist folder is the place to look for the static files*/
  app.use(express.static(path.join(__dirname, 'dist')));
